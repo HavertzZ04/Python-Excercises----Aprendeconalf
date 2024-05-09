d247fe986a2793c61dc7eeb574c89c29ef9797d5
havertzz04 <havertzz04@gmail.com>
2024-05-08 20:47:00 -0500

    feat: :beers: Lists

--- tuples and lists/12.py
+++ tuples and lists/12.py
@@ -5,14 +5,19 @@ Escribir un programa que almacene las matrices
 en una lista y muestre por pantalla su producto.
 Nota: Para representar matrices mediante listas usar listas anidadas, representando cada vector fila en una lista.
 '''
+import numpy as np
 
-#fila = i , columna = j => AB
+A = np.array([
+     [1, 2 ,3],
+     [4, 5, 6]
+])
 
-
-
-a = [[1, 2 ,3],
-     [4, 5, 6]]
-
-b = [[-1, 0],
+B = np.array([
+     [-1, 0],
      [0, 1],
-     [1, 1]]
+     [1, 1]
+])
+
+C = np.dot(A, B)
+
+print(C)    
